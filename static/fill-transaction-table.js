28941b3d30df44146296a89cf3ecb323bae0a52a
Erwan Lemonnier <erwan@spotify.com>
2015-01-30 00:27:26 +0100

    Split-out javascript for listing transactions + prototype buttons
    Prototype buttons with table popdown listing months of the year.
    To be populated dynamically via javascript

---
+++ static/fill-transaction-table.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://127.0.0.1:8080/transactions/get/2014-08'
+    }).then(function(data) {
+        $('.state-log').append('started<br>')
+        $('.state-log').append('got '+data.transactions.length+' rows<br>')
+        if(data){
+            transactions = data.transactions;
+            var len = transactions.length;
+            var txt = "";
+            if(len > 0){
+                for(var i=0;i<len;i++){
+                    t = transactions[i]
+                    txt += "<tr><td>"+i+"</td><td>"+t.date+"</td><td>"+t.amount+"</td><td>"+t.target+"</td><td>"+t.owner+"</td><td>"+t.category+"</td></tr>";
+                }
+            }
+            if(txt != ""){
+                $('.table-content').append(txt);
+            }
+        }
+    });
+});
