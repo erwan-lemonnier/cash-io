4da0cf95fcc70c4a0286603503004aace5ef2298
erwan <erwan@spotify.com>
2015-06-01 16:14:31 +0200

    Show monthly spendings and balance with bar charts

--- static/fill-transaction-table.js
+++ static/fill-transaction-table.js
@@ -4,9 +4,8 @@ function load_monthly_transactions(year, month) {
             type: 'GET',
             url: 'http://127.0.0.1:8080/api/v0/transactions/get/' + year + '-' + month
         }).then(function(data) {
-            $('.state-log').append('started<br>')
-            $('.state-log').append('got '+data.transactions.length+' rows<br>')
             if(data){
+                // Fill the table with transactions
                 var transactions = data.transactions;
                 var len = transactions.length;
                 var txt = "";
@@ -20,31 +19,59 @@ function load_monthly_transactions(year, month) {
                     $('.table-content').empty().append(txt);
                 }
 
-                // Build input data for the polar area chart
+                // Build a linechart showing amount spent for each category
                 var sums = data.categories;
-                var chartdata = new Array();
-                $('.table-content').empty();
-                for (var cat in sums) {
-                    amount = sums[cat]
-                    $('.table-content').append('checking ' + cat + ' ' + amount + '<br>');
+                var mylabels = [];
+                var mydatas = [];
+
+                var keys = [];
+                for (var k in sums) {
+                    keys.push(k);
+                }
+                keys = keys.sort();
+
+                for (var i in keys) {
+                    var cat = keys[i];
+                    amount = sums[cat];
                     if (amount <= 0) {
-                        var d = {
-                            value: Math.abs(Math.round(amount)),
-                            label: cat,
-                            color:"#F7464A",
-                            highlight:"#F7464A",
-                        }
-                        chartdata.push(d)
+                        mylabels.push(cat);
+                        mydatas.push(Math.abs(amount));
                     }
                 }
 
-                var output = '';
-                for (var property in chartdata) {
-                    output += chartdata[property]['label'] + ': ' + chartdata[property]['value'] +'; ';
+                var chartdata = {
+                    labels: mylabels,
+                    datasets: [
+                        {
+                            label: "Spent by category",
+                            fillColor: "rgba(151,187,205,0.5)",
+                            strokeColor: "rgba(151,187,205,0.8)",
+                            highlightFill: "rgba(151,187,205,0.75)",
+                            highlightStroke: "rgba(151,187,205,1)",
+                            data: mydatas,
+                        }
+                    ],
                 }
-                $('.table-content').empty().append('chartdata: ' + output);
-                var ctx = $("#monthly_transactions").get(0).getContext("2d");
-                var chart = new Chart(ctx).PolarArea(chartdata, {animationSteps : 5});
+
+                var ctx1 = $("#spendings").get(0).getContext("2d");
+                var chart1 = new Chart(ctx1).Bar(chartdata, {animationSteps: 1});
+
+                // And a linechart showing the earned vs. spent balance
+                balancedata = {
+                    labels: ['earned', 'spent'],
+                    datasets: [
+                        {
+                            label: "Balance",
+                            fillColor: "rgba(151,187,205,0.5)",
+                            strokeColor: "rgba(151,187,205,0.8)",
+                            highlightFill: "rgba(151,187,205,0.75)",
+                            highlightStroke: "rgba(151,187,205,1)",
+                            data: [Math.abs(data.total_earned), Math.abs(data.total_spent)],
+                        }
+                    ],
+                }
+                var ctx2 = $("#balance").get(0).getContext("2d");
+                var chart2 = new Chart(ctx2).Bar(balancedata, {animationSteps: 1});
             }
         });
     });
