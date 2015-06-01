79bf69b13f18193ed45fd4befc69d5ffb4de18e9
erwan <erwan@spotify.com>
2015-06-01 14:26:36 +0200

    Show monthly transactions for a given selected month + refactorings

--- static/fill-transaction-table.js
+++ static/fill-transaction-table.js
@@ -1,23 +1,25 @@
-$(document).ready(function() {
-    $.ajax({
-        type: 'GET',
-        url: 'http://127.0.0.1:8080/api/v0/transactions/get/2014-08'
-    }).then(function(data) {
-        $('.state-log').append('started<br>')
-        $('.state-log').append('got '+data.transactions.length+' rows<br>')
-        if(data){
-            transactions = data.transactions;
-            var len = transactions.length;
-            var txt = "";
-            if(len > 0){
-                for(var i=0;i<len;i++){
-                    t = transactions[i]
-                    txt += "<tr><td>"+i+"</td><td>"+t.date+"</td><td>"+t.amount+"</td><td>"+t.target+"</td><td>"+t.owner+"</td><td>"+t.category+"</td></tr>";
+function load_monthly_transactions(year, month) {
+    $(document).ready(function() {
+        $.ajax({
+            type: 'GET',
+            url: 'http://127.0.0.1:8080/api/v0/transactions/get/' + year + '-' + month
+        }).then(function(data) {
+            $('.state-log').append('started<br>')
+            $('.state-log').append('got '+data.transactions.length+' rows<br>')
+            if(data){
+                transactions = data.transactions;
+                var len = transactions.length;
+                var txt = "";
+                if(len > 0){
+                    for(var i=0;i<len;i++){
+                        t = transactions[i]
+                        txt += "<tr><td>"+i+"</td><td>"+t.date+"</td><td>"+t.amount+"</td><td>"+t.target+"</td><td>"+t.owner+"</td><td>"+t.category+"</td></tr>";
+                    }
+                }
+                if(txt != ""){
+                    $('.table-content').empty().append(txt);
                 }
             }
-            if(txt != ""){
-                $('.table-content').append(txt);
-            }
-        }
+        });
     });
-});
+}
