ed3cdebfd8fc31095b8b5e4fd58fb40c94dfc8e1
erwan <erwan@spotify.com>
2015-06-05 16:18:45 +0200

    Preparing stage for multi-years reports

--- static/cashio.js
+++ static/cashio.js
@@ -48,8 +48,8 @@ function show_balance_chart(elem_name, total_earned, total_spent) {
     data.addColumn('string', 'Type');
     data.addColumn('number', 'Amount');
     data.addRows([
-        ['Earned', total_earned],
-        ['Spent', total_spent],
+        ['Earned', Math.abs(total_earned)],
+        ['Spent', Math.abs(total_spent)],
     ]);
 
     var options = {'title':'Earned/Spent'};
@@ -72,8 +72,38 @@ function fill_transaction_table(table, transactions) {
     }
 }
 
-function load_monthly_transactions(year, month) {
+function show_repartition_by_year(elem_name, year, sum_by_category) {
+    titles = ["Year"]
+    values = [year]
+    for(var cat in sum_by_category) {
+        titles.push(cat);
+        values.push(sum_by_category[cat]);
+    }
 
+    var data = google.visualization.arrayToDataTable([titles, values]);
+
+    //TODO: set colors to fix values, in same order as categories, to assign color to cats
+    var options = {
+        title: 'Spendings by year',
+        isStacked: true,
+        connectSteps: false,
+        legend: {
+            //position: 'bottom',
+            position: 'top',
+            maxLines: 5,
+        },
+        height: 1000,
+        vAxis: {
+            title: 'Money spent',
+            format: 'currency',
+        }
+    };
+
+    var chart = new google.visualization.SteppedAreaChart(document.getElementById(elem_name));
+    chart.draw(data, options);
+}
+
+function load_monthly_transactions(year, month) {
     $(document).ready(function() {
         $.ajax({
             type: 'GET',
@@ -91,4 +121,30 @@ function load_monthly_transactions(year, month) {
             }
         });
     });
+}
+
+function load_yearly_transactions(year) {
+    $(document).ready(function() {
+        $.ajax({
+            type: 'GET',
+            url: 'http://127.0.0.1:8080/api/v0/transactions/get/' + year
+        }).then(function(data) {
+            set_title($('.page-header'), year);
+            if(data){
+                show_repartition_by_year('repartition', year, data.categories);
+
+                var ctx1 = $("#spendings").get(0).getContext("2d");
+                show_sum_by_category_chart(ctx1, data.categories);
+
+                show_balance_chart('chart_balance', data.total_earned, data.total_spent);
+            }
+        });
+    });
+}
+
+function load_many_years_transactions(years) {
+    //load_yearly_transactions(years[0]);
+    set_title($('.page-header'), years);
+    // TODO: and now, get transactions for multiple years in one shot!!
+    // see http://stackoverflow.com/questions/17719344/multiple-rest-calls-best-practise
 }
